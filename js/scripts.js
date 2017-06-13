b1649e9d3bc819782bfcba4569687a56929c4e8b
Nathan Harrenstein <nathan.harrenstein@gmail.com>
2017-06-13 10:10:47 -0700

    Added constructor functions to model website's data.

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+function codeSample(title, image, description) {
+  this.title = title;
+  this.image = image;
+  this.description = description;
+}
+
+function project(title, image, link, description) {
+  this.title = title;
+  this.image = image;
+  this.link = link;
+  thils.description = description;
+}
