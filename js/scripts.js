9a70a83b463b8d8ceaae56bb0601da700278cb5e
Nathan Harrenstein <nathan.harrenstein@gmail.com>
2017-06-13 21:40:13 -0700

    Added hamburger menu functionality and styling.

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,25 @@
-function codeSample(title, image, description) {
-  this.title = title;
-  this.image = image;
-  this.description = description;
-}
-
-function project(title, image, link, description) {
-  this.title = title;
-  this.image = image;
-  this.link = link;
-  this.description = description;
-}
+// function codeSample(title, image, description) {
+//   this.title = title;
+//   this.image = image;
+//   this.description = description;
+// }
+//
+// function project(title, image, link, description) {
+//   this.title = title;
+//   this.image = image;
+//   this.link = link;
+//   this.description = description;
+// }
 
 $(document).ready(function() {
-  $('.icon-menu').mouseenter(function (){
-    $('nav ul').display = 'block';
-  });
+  $('.icon-menu').mouseenter(openMenu);
+  $('.menu-close-button').click(closeMenu);
 });
+
+function openMenu() {
+  $('nav').css('display','block');
+}
+
+function closeMenu() {
+  $('nav').css('display','none');
+}
