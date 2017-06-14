b875fc3183b8ea66f81a6da8965502c38bc86d8f
Nathan Harrenstein <nathan.harrenstein@gmail.com>
2017-06-14 11:27:48 -0700

    Added project template.

---
+++ js/models.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// function codeSample(title, image, description) {
+//   this.title = title;
+//   this.image = image;
+//   this.description = description;
+// }
+//
+// function project(title, image, link, description) {
+//   this.title = title;
+//   this.image = image;
+//   this.link = link;
+//   this.description = description;
+// }
--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,5 @@
 'use strict';
 
-// function codeSample(title, image, description) {
-//   this.title = title;
-//   this.image = image;
-//   this.description = description;
-// }
-//
-// function project(title, image, link, description) {
-//   this.title = title;
-//   this.image = image;
-//   this.link = link;
-//   this.description = description;
-// }
-
 $(document).ready(function() {
   $('.icon-menu').mouseenter(openMenu);
   $('.menu-close-button').click(closeMenu);
