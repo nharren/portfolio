e12eae8d2465492c060617d20d71a71cf4f144d5
Nathan Harrenstein <nathan.harrenstein@gmail.com>
2017-06-18 13:07:14 -0700

    Use matchMedia function to conditionally subscribe to mobile menu functionality.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,9 +1,19 @@
 'use strict';
 
 $(document).ready(function() {
-  $('.icon-menu').on('click', toggleMenu);
-  $('.menu-item').on('click', toggleMenu);
+  var mediaQueryList = window.matchMedia('(min-width: 640px)')
+  mediaQueryList.addListener(handleEvents)
 });
+
+function handleEvents(event) {
+  if (event.matches) {
+    $('.icon-menu').off('click', toggleMenu);
+    $('.menu-item').off('click', toggleMenu);   
+  } else {
+    $('.icon-menu').on('click', toggleMenu);
+    $('.menu-item').on('click', toggleMenu);
+  }
+}
 
 function toggleMenu() {
   $('main').toggle();
