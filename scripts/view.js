76c740bd149083009260a2ea96dc42a72cc2cb0f
Nathan Harrenstein <nathan.harrenstein@gmail.com>
2017-06-19 22:31:35 -0700

    Implemented slideshow animation with menu.

--- scripts/view.js
+++ scripts/view.js
@@ -4,16 +4,38 @@ var projectView = {};
 
 projectView.handleMainNav = function() {
   $('.menu-item').on('click', function(){
+    $('.tab-content').removeClass('slideLeft slideRight');
+    $('.tab-content').removeAttr('style');
+
     var $previousTab = $('.selected');
-    var $previousTabContent = $('#' + $previousTab.data('content'));
     var $newTab = $(this);
+
+    var $previousTabContent = $('#' + $previousTab.data('content'));
     var $newTabContent = $('#' + $newTab.data('content'))
 
     $previousTab.removeClass('selected');
     $newTab.addClass('selected');
 
-    $previousTabContent.addClass('hidden');
     $newTabContent.removeClass('hidden');
+
+    var left;
+
+    if ($newTab.index() > $previousTab.index()) {
+      $newTabContent.addClass('slideLeft');
+      left = '-200%';
+    } else {
+      $newTabContent.addClass('slideRight');
+      left = '200%';
+    }
+
+    $previousTabContent.animate({left: left}, {
+      queue: false, 
+      complete: function() {
+        $previousTabContent.addClass('hidden');
+      }
+    });
+
+    $newTabContent.animate({left: '0'});
   });
 };
 
