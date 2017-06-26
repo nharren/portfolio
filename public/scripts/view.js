1d7d695f005c1a0151a014354a07c8e1294f8e11
Nathan Harrenstein <nathan.harrenstein@gmail.com>
2017-06-26 10:29:56 -0700

    Wrap javascript in IFFE.

--- public/scripts/view.js
+++ public/scripts/view.js
@@ -1,134 +1,140 @@
 'use strict';
 
-var projectView = {};
+var app = app || {};
 
-projectView.handleMainNav = function() {
-  $('.menu-item').on('click', function(){
-    if ($(this).hasClass('selected')) {
-      return;
-    }
+(function(module) {
+  var projectView = {};
 
-    $('.tab-content').removeAttr('style');
-
-    var $newTab = $(this);
-    var $previousTab = $('.selected');
-
-    var $previousTabContent = $('#' + $previousTab.data('content'));
-    var $newTabContent = $('#' + $newTab.data('content'))
-
-    $previousTab.removeClass('selected');
-    $newTab.addClass('selected');
-
-    $newTabContent.removeClass('hidden');
-
-    if (window.innerWidth > 640) {
-      var left;
-
-      if ($newTab.index() > $previousTab.index()) {
-        $newTabContent.addClass('slideLeft');
-        left = '-100vw';
-      } else {
-        $newTabContent.addClass('slideRight');
-        left = '100vw';
+  projectView.handleMainNav = function() {
+    $('.menu-item').on('click', function(){
+      if ($(this).hasClass('selected')) {
+        return;
       }
 
-      $previousTabContent.animate({left: left}, '1s', function() {
+      $('.tab-content').removeAttr('style');
+
+      var $newTab = $(this);
+      var $previousTab = $('.selected');
+
+      var $previousTabContent = $('#' + $previousTab.data('content'));
+      var $newTabContent = $('#' + $newTab.data('content'))
+
+      $previousTab.removeClass('selected');
+      $newTab.addClass('selected');
+
+      $newTabContent.removeClass('hidden');
+
+      if (window.innerWidth > 640) {
+        var left;
+
+        if ($newTab.index() > $previousTab.index()) {
+          $newTabContent.addClass('slideLeft');
+          left = '-100vw';
+        } else {
+          $newTabContent.addClass('slideRight');
+          left = '100vw';
+        }
+
+        $previousTabContent.animate({left: left}, '1s', function() {
+          $previousTabContent.addClass('hidden');
+        });
+
+        $newTabContent.animate({left: '0'}, function(){
+          $newTabContent.removeClass('slideLeft slideRight');
+        });
+      } else {
         $previousTabContent.addClass('hidden');
-      });
+      }
+    });
+  };
 
-      $newTabContent.animate({left: '0'}, function(){
-        $newTabContent.removeClass('slideLeft slideRight');
-      });
-    } else {
-      $previousTabContent.addClass('hidden');
-    }
-  });
-};
+  projectView.setTeasers = function() {
+    $('.project-details').hide();
 
-projectView.setTeasers = function() {
-  $('.project-details').hide();
+    $('#projects').on('click','.project-header', function(event) {
+      var $projectHeader = $(this);
+      $projectHeader.prev('.project-details').slideToggle();
 
-  $('#projects').on('click','.project-header', function(event) {
-    var $projectHeader = $(this);
-    $projectHeader.prev('.project-details').slideToggle();
+      var $arrow = $projectHeader.children('.project-title-arrow');
+      $arrow.toggleClass('expanded');
+      $arrow.toggleClass('collapsed');
 
-    var $arrow = $projectHeader.children('.project-title-arrow');
-    $arrow.toggleClass('expanded');
-    $arrow.toggleClass('collapsed');
+      event.preventDefault();
+    });
+  };
 
-    event.preventDefault();
-  });
-};
-
-projectView.setTheme = function() {
-  var darkTheme = $('<link rel="stylesheet" href="styles/themes/dark.css">');
-  if (new Date().getHours() > 17) {
-    $('head').append(darkTheme);
-  }
-
-  $('.icon-contrast').on('click', function() {
-    let $foundDarkTheme = $('link[href="styles/themes/dark.css"]');
-    if ($foundDarkTheme.length) {
-      $foundDarkTheme.remove();
-    } else {
+  projectView.setTheme = function() {
+    var darkTheme = $('<link rel="stylesheet" href="styles/themes/dark.css">');
+    if (new Date().getHours() > 17) {
       $('head').append(darkTheme);
     }
-  })
-};
 
-projectView.populateFilter = function() {
-  var $filter = $('.filter-dropdown');
-  var technologies = [];
-  Project.all.forEach(function(project) {
-    project.technologies.forEach(function(technology) {
-      technologies.push(technology);
-    });
-  });
+    $('.icon-contrast').on('click', function() {
+      let $foundDarkTheme = $('link[href="styles/themes/dark.css"]');
+      if ($foundDarkTheme.length) {
+        $foundDarkTheme.remove();
+      } else {
+        $('head').append(darkTheme);
+      }
+    })
+  };
 
-  technologies.sort();
-  technologies = new Set(technologies);
-
-  technologies.forEach(function(technology) {
-    $filter.append($(`<div class="filter-item">${technology}</div>`));
-  });
-}
-
-projectView.handleFilter = function() {
-  $('.filter-selection').on('click', function() {
-    $('.filter-dropdown').slideToggle(200);
-  });
-
-  $('.filter-item').on('mousedown', function(){
-    $('.filter-dropdown').toggle();
-    var technology = $(this).get(0).innerText;
-    if (!technology.startsWith('--')) {
-      $('.project').hide();
-      $('.project').each(function() {
-        $(this).find('.project-technology').get().forEach(s => {
-          if (s.innerText.includes(technology)) {
-            $(this).fadeIn();
-          }
-        });
+  projectView.populateFilter = function() {
+    var $filter = $('.filter-dropdown');
+    var technologies = [];
+    app.Project.all.forEach(function(project) {
+      project.technologies.forEach(function(technology) {
+        technologies.push(technology);
       });
-    } else {
-      $('.project').fadeIn();
-    }
+    });
 
-    $('.filter-selection').text(technology);
-  });
-}
+    technologies.sort();
+    technologies = new Set(technologies);
 
-projectView.addProjects = function() {
-  Project.all.forEach(function(project){
-    $('#projects').append(project.toHtml());
-  });
-}
+    technologies.forEach(function(technology) {
+      $filter.append($(`<div class="filter-item">${technology}</div>`));
+    });
+  }
 
-projectView.init = function() {
-  projectView.setTheme();
-  projectView.setTeasers();
-  projectView.handleMainNav();
-  projectView.addProjects();
-  projectView.populateFilter();
-  projectView.handleFilter();
-};
+  projectView.handleFilter = function() {
+    $('.filter-selection').on('click', function() {
+      $('.filter-dropdown').slideToggle(200);
+    });
+
+    $('.filter-item').on('mousedown', function(){
+      $('.filter-dropdown').toggle();
+      var technology = $(this).get(0).innerText;
+      if (!technology.startsWith('--')) {
+        $('.project').hide();
+        $('.project').each(function() {
+          $(this).find('.project-technology').get().forEach(s => {
+            if (s.innerText.includes(technology)) {
+              $(this).fadeIn();
+            }
+          });
+        });
+      } else {
+        $('.project').fadeIn();
+      }
+
+      $('.filter-selection').text(technology);
+    });
+  }
+
+  projectView.addProjects = function() {
+    app.Project.all.forEach(function(project){
+      $('#projects').append(project.toHtml());
+    });
+  }
+
+  projectView.init = function() {
+    projectView.setTheme();
+    projectView.setTeasers();
+    projectView.handleMainNav();
+    projectView.addProjects();
+    projectView.populateFilter();
+    projectView.handleFilter();
+  };
+
+  module.projectView = projectView;
+})(app);
